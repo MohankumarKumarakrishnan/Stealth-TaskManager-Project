2a049fafcca6c1c6285fd6c6c25bbc4db0e81b98
sarav <mkmohankumar261998@gmail.com>
2025-05-08 11:53:25 +0530

    Fixed deploy issue

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,11 @@ app.use('/api',userAuthRoute)
 app.use('/api',tasksRoute)
 
 if(process.env.ENVIRONMENT === "PRODUCTION"){
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 }
 
 
